0d9e6f86d10334d9f3bdf743d3cf5487d2b1627a
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-09-28 14:50:37 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,3 @@
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import { Provider } from 'react-redux'
-// import { store } from './config/redux/store/store.js'
-
-// createRoot(document.getElementById('root')).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// )
-
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
